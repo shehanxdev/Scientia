803e94db86fe49797547e48ac1f3ffc8e4630df5
Shehan Chanuka <shehanx15@gmail.com>
2021-11-24 02:12:00 +0530

    version date: 24/22/2021 time:2:11 AM comment: menu is alomst done

--- javascript/index.js
+++ javascript/index.js
@@ -7,5 +7,19 @@ function calc() {
   let header_height = document.querySelector(".shadow").offsetHeight;
   let siderbar_height = window.innerHeight - header_height;
   //applying calculated height to the sidebar
-  document.styleSheets[2].cssRules[2].style.height = siderbar_height + "px";
+  document.styleSheets[2].cssRules[9].style.height = siderbar_height + "px";
+}
+
+$(".custom-css-hamburger").click(toogleMenu);
+$(".custom-css-close").click(toogleMenu);
+
+function toogleMenu() {
+  $(".custom-css-hamburger").toggleClass("custom-css-hamburger-off");
+  $(".custom-css-close").toggleClass("custom-css-close-on");
+  if ($(".custom-css-close").hasClass("custom-css-close-on")) {
+    document.querySelector(".custom-css-close-wrapper").style.zIndex = 1010;
+  } else {
+    document.querySelector(".custom-css-close-wrapper").style.zIndex = 900;
+  }
+  $(".custom-css-sidebar").toggleClass("custom-css-sidebar-on");
 }
