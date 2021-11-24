d96aa7f50c28c9d92c2105a8066f3a86c2a79fd0
Shehan Chanuka <shehanx15@gmail.com>
2021-11-24 05:42:08 +0530

    version date:24/11/2021 time:5:41AM comment:Main page is almost dom=ne with some minor adjustments to be made. Footer is not designed yet

--- javascript/index.js
+++ javascript/index.js
@@ -6,8 +6,9 @@ function calc() {
   //calculating the height of the sidebar
   let header_height = document.querySelector(".shadow").offsetHeight;
   let siderbar_height = window.innerHeight - header_height;
+
   //applying calculated height to the sidebar
-  document.styleSheets[2].cssRules[9].style.height = siderbar_height + "px";
+  document.styleSheets[2].cssRules[10].style.height = siderbar_height + "px";
 }
 
 $(".custom-css-hamburger").click(toogleMenu);
@@ -17,9 +18,19 @@ function toogleMenu() {
   $(".custom-css-hamburger").toggleClass("custom-css-hamburger-off");
   $(".custom-css-close").toggleClass("custom-css-close-on");
   if ($(".custom-css-close").hasClass("custom-css-close-on")) {
-    document.querySelector(".custom-css-close-wrapper").style.zIndex = 1010;
+    document.querySelector(".custom-css-close-wrapper").style.zIndex = 1100;
+    document.querySelector(".custom-css-hamburger").style.zIndex = 900;
+    document.querySelector(".custom-css-sidebar").style.zIndex = 1000;
+    document.querySelector(
+      ".custom-css-post-preview-section "
+    ).style.zIndex = 900;
   } else {
     document.querySelector(".custom-css-close-wrapper").style.zIndex = 900;
+    document.querySelector(".custom-css-hamburger").style.zIndex = 1000;
+    document.querySelector(".custom-css-sidebar").style.zIndex = 900;
+    document.querySelector(
+      ".custom-css-post-preview-section "
+    ).style.zIndex = 1000;
   }
   $(".custom-css-sidebar").toggleClass("custom-css-sidebar-on");
 }
