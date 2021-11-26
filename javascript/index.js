c0eb3badb6e761f3515a6a66dce33a7a95bb9b5e
Shehan Chanuka <shehanx15@gmail.com>
2021-11-26 12:01:53 +0530

    Version date:26/11/2021 time:12:01 PM comment: Home page is almost done

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+//global variables
+let hamburger_counter = 0;
 //adding event listners to the window object
 window.addEventListener("load", calc);
 
@@ -8,7 +10,7 @@ function calc() {
   let siderbar_height = window.innerHeight - header_height;
 
   //applying calculated height to the sidebar
-  document.styleSheets[2].cssRules[10].style.height = siderbar_height + "px";
+  document.styleSheets[3].cssRules[14].style.height = siderbar_height + "px";
 }
 
 $(".custom-css-hamburger").click(toogleMenu);
@@ -16,21 +18,29 @@ $(".custom-css-close").click(toogleMenu);
 
 function toogleMenu() {
   $(".custom-css-hamburger").toggleClass("custom-css-hamburger-off");
+  if (hamburger_counter == 0) {
+    $(".custom-css-hamburger").toggleClass("custom-css-hamburger-animate");
+    ++hamburger_counter;
+  }
   $(".custom-css-close").toggleClass("custom-css-close-on");
   if ($(".custom-css-close").hasClass("custom-css-close-on")) {
     document.querySelector(".custom-css-close-wrapper").style.zIndex = 1100;
-    document.querySelector(".custom-css-hamburger").style.zIndex = 900;
+    document.querySelector(".custom-css-hamburger-wrapper").style.zIndex = 900;
     document.querySelector(".custom-css-sidebar").style.zIndex = 1000;
     document.querySelector(
       ".custom-css-post-preview-section "
     ).style.zIndex = 900;
   } else {
     document.querySelector(".custom-css-close-wrapper").style.zIndex = 900;
-    document.querySelector(".custom-css-hamburger").style.zIndex = 1000;
+    document.querySelector(".custom-css-hamburger-wrapper").style.zIndex = 1000;
     document.querySelector(".custom-css-sidebar").style.zIndex = 900;
     document.querySelector(
       ".custom-css-post-preview-section "
     ).style.zIndex = 1000;
   }
-  $(".custom-css-sidebar").toggleClass("custom-css-sidebar-on");
+  if (window.innerWidth > 768) {
+    $(".custom-css-sidebar").toggleClass("custom-css-sidebar-on-desktop");
+  } else {
+    $(".custom-css-sidebar").toggleClass("custom-css-sidebar-on");
+  }
 }
